48adc0edc0d0677a2f88d91627b28024a3e9828f
mabosaimi <132561114+mabosaimi@users.noreply.github.com>
2026-02-15 05:21:16 +0300

    init web with tailwind, biome and boilerplate react

---
+++ web/src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+	return (
+		<main>
+			<h1 className="animate-pulse font-bold text-4xl text-blue-500 text-center pt-48">
+				Chess Bots Museum
+			</h1>
+		</main>
+	);
+}
+
+export default App;
